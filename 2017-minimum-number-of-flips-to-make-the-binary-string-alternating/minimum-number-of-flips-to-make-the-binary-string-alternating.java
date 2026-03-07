856679b256363e051f50bc08af27771c0e5999c0
Chandrikagollashetti <chandrikagollashetti@gmail.com>
2026-03-07 09:54:38 +0530

    Time: 21 ms (85.17%) | Memory: 47.4 MB (52.54%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,45 +1,35 @@
 class Solution {
     public int minFlips(String s) {
-        int n = s.length();
-        String str = s + s;
-
-        int diff1 = 0;
-        int diff2 = 0;
-        int ans = Integer.MAX_VALUE;
-
-        for(int i = 0; i < str.length(); i++){
-
-            char c = str.charAt(i);
-
-            if(i % 2 == 0){
-                if(c != '0') diff1++;
-                if(c != '1') diff2++;
+        int n=s.length();
+        String str=s+s;
+        int c1=0;
+        int c2=0;
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            if(i%2==0){
+                if(c!='0')c1++;
+                if(c!='1')c2++;
             }
             else{
-                if(c != '1') diff1++;
-                if(c != '0') diff2++;
+                if(c!='1')c1++;
+                if(c!='0')c2++;
             }
-
-            if(i >= n){
-                char old = str.charAt(i-n);
-
+            if(i>=n){
+                char old=str.charAt(i-n);
                 if((i-n)%2==0){
-                    if(old != '0') diff1--;
-                    if(old != '1') diff2--;
+                    if(old!='0') c1--;
+                    if(old!='1') c2--;
                 }
                 else{
-                    if(old != '1') diff1--;
-                    if(old != '0') diff2--;
+                    if(old!='1')c1--;
+                    if(old!='0')c2--;
                 }
             }
-
-            if(i >= n-1){
-                ans = Math.min(ans, Math.min(diff1, diff2));
+            if(i>=n-1){
+                ans=Math.min(ans,Math.min(c1,c2));
             }
         }
-
         return ans;
-
-
     }
 }
