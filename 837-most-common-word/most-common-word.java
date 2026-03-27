744da390952994e16115c2da48b2b318f052f28b
Chandrikagollashetti <chandrikagollashetti@gmail.com>
2026-03-27 07:12:27 +0530

    Time: 17 ms (49.34%) | Memory: 45.5 MB (16.93%) - LeetSync

---
+++ 837-most-common-word/most-common-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph=paragraph.toLowerCase().replaceAll("[^a-z]"," ");
+        String[] words=paragraph.split(" ");
+        Set<String> b=new HashSet<>(Arrays.asList(banned));
+        HashMap<String,Integer> hm=new HashMap<>();
+        String ans="";
+        int m=0;
+        for(String word:words){
+            if(!b.contains(word) && word.length()>0){
+                hm.put(word,hm.getOrDefault(word,0)+1);
+                if(hm.get(word)>m){
+                    m=hm.get(word);
+                    ans=word;
+                }
+            }
+        }
+        return ans;
+    }
+}
